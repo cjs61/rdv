35df404d697a126399d68afa0e8732d89f52514d
Unknown <barre.js@gmail.com>
2018-11-29 12:00:52 +0100

    Organisation de rencontres
    tuto : https://www.youtube.com/watch?v=FXY1UyQfSFw&index=1&list=PLf5DGoTIOmPa8U83XWOLPpJ0eCz8LYKea 
    
    En ligne de commande : 
    Cd Projet-vuetify
    Vue init webpack meetup
    Entrer (par default)
    Entrer (par default)
    Yes
    Rooter yes 
    No…
    
    Cd meetup
    
    Npm install vuetify
    
    Npm run dev 
    puis dans le navigateur, aller sur localhost:8080
    
    
    j'ajoute le font dans index.html (à la fin du head)
    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
    
    Dans main.js :
    import Vuetify from 'vuetify';
    import 'vuetify/dist/vuetify.min.css';
    
    Et sous vue.config…
    Vue.use(Vuetify);
    
    À partir de l’adresse <https://vuetifyjs.com/en/getting-started/quick-start> 
    je colle le template qui contient le drawer dans app.vue
    je personnalise la toolbar
    
    remarque pour mettre une icone à un bouton, je place mes balises <v-icon> entre les balises <v-btn>

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+<v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app>
+
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+            <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+
+ <v-toolbar>
+  <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+  <v-toolbar-title>DevMeetup</v-toolbar-title>
+  
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-xs-only">
+    <v-btn flat>
+      <v-icon left>people</v-icon>
+      Vue Meetups</v-btn>
+  </v-toolbar-items>
+</v-toolbar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+</v-app>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      drawer: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import Vuetify from 'vuetify';
+
+import 'vuetify/dist/vuetify.min.css';
+import router from './router'
+
+Vue.config.productionTip = false
+Vue.use(Vuetify);
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
