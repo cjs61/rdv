d9ca304d48b913a52afc5be6767b874b1aaf2e44
Unknown <barre.js@gmail.com>
2018-12-01 16:36:22 +0100

    CreateMeetup
    pb pas de vue

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        <p>The Create Meetup Page</p>
-    </div>
+   <v-container>
+       <v-layout row>
+           <v-flex xs12>
+               <h2 class="primary--text">Create a new Meetup</h2>
+           </v-flex>
+        </v-layout> 
+        <v-layout row>
+            <v-flex xs12>
+                 <v-form v-model="valid">
+                    <v-layout row>
+                        <v-flex xs12 sm6 offset-sm3>
+                            <v-text-field
+                                name="title"
+                                label="Titre"
+                                id="title"
+                                required>
+                            </v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-form>
+            </v-flex>
+        </v-layout>
+   </v-container>
 </template>
