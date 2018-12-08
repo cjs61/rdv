dfb42cf69660c9bf244eb480cbe536494402aa79
Unknown <barre.js@gmail.com>
2018-12-08 21:22:42 +0100

    page signIn
    je recopie le code de signUp
    j'enlève le confirmPassword
    j'ajoute la méthode onSignIn dans index.js de store

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,86 @@
 <template>
-    <div>
-        <p>The Sign In Page</p>
-    </div>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <!-- création d'une grid -->
+                        <v-container>
+                            <!-- j'enlève action= car je ne veux pas envoyer de request -->
+                            <!-- je ne mets pas les parenthèses à onSignup car seul un pointer suffit -->
+                            <form @submit.prevent="onSignin">
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                            name="email"
+                                            label="Mail"
+                                            id="email"
+                                            v-model="email"
+                                            type="email"
+                                            required>
+                                        </v-text-field>
+                                     </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                        name="password"
+                                        label="Password"
+                                        id="password"
+                                        v-model="password"
+                                        type="password"
+                                        required>
+                                        </v-text-field>
+                                     </v-flex>
+                                </v-layout>
+                                
+                                <v-layout>
+                                    <v-flex>
+                                        <v-btn type="submit">Sign in</v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    computed: {
+        
+        user () {
+            // je renvoie le user que je viens de créer
+            return this.$store.getters.user
+        }
+    },
+    // regarde le user computed propriété
+    watch: {
+        // je stocke une valeur à chaque fois que le statut du user change
+        user (value) {
+            // je vérifie si le user existe
+            if (value !== null && value !== undefined) {
+                // je redirige vers la home page
+                this.$router.push('/')
+            }
+        }
+    },
+    methods: {
+        onSignin () {
+        // vuex
+        this.$store.dispatch('signUserIn', {email: this.email, password: this.password })
+        }
+    }
+    
+}
+</script>
